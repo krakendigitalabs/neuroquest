37984f9b8601d92f6eb725b7ca53ecaf9c81fb5d
Kraken Digital Labs <krakendigitalabs@gmail.com>
2026-03-14 20:00:23 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3083535459-c0472",
+  "appId": "1:722909026858:web:bee45467f78d760bea4153",
+  "apiKey": "AIzaSyC4vMUPG2wIV5FCmgmmwbCZdR4s1BxEMDc",
+  "authDomain": "studio-3083535459-c0472.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "722909026858"
+};
